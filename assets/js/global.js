f028066fb903adf97edce6ce7b8a257132a6cdb9
juan david bolivar <judaob95@gmail.com>
2020-10-27 20:56:25 -0500

    cambios

---
+++ assets/js/global.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    /* ===========================================================
+     main scroll
+    =============================================================*/
+
+    $(window).scroll(function() {
+        var winTop = $(window).scrollTop();
+        if (winTop >= 500) {
+            $("nav").addClass("sticky-top");
+        } else {
+            $("nav").removeClass("sticky-top");
+        }
+    });
+
+
+    /* ===========================================================
+     carousel
+    =============================================================*/
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        // autoplay: true,
+        // autoplayHoverPause: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+})
